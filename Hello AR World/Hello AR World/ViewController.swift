9c03ef0576756e4237326cf7829c40d252c93c7b
Jacqualyn Blizzard <jacqualyn.blizzard@gmail.com>
2019-03-05 16:47:20 -0700

    Add files via upload

---
+++ Hello AR World/Hello AR World/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  Hello AR World
+//
+//  Created by Jacqualyn Blizzard-Caron on 3/5/19.
+//  Copyright © 2019 Jacqualyn Blizzard-Caron. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // Define the text
+        let text = SCNText(string: "Hello AR World", extrusionDepth: 1)
+        // Set the color of the text by creating a material object
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.purple
+        text.materials = [material]
+        
+        // Set the position of the text
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0.0, y: 0.02, z: -0.1)
+        node.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
+        node.geometry = text
+        
+        //Add to the camera image
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
